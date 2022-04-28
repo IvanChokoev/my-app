54062bde28b139cb0e7119f2cc0c780e786b980e
Ivan Chokoev <ivanchokoev96@gmail.com>
2022-04-28 14:23:59 +0300

    Fixed some minor bugs with the "buy" button.

---
+++ src/app/cart.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './products';
+
+/* The addToCart() method appends a product to an array of items
+The getItems() method collects the items users add to the cart and returns each item with its associated quantity
+The clearCart() method returns an empty array of items, which empties the cart */
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  items: Array<Product> = [];
+
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get<{ type: string, price: number }[]>('/assets/shipping.json');
+  }
+
+  constructor(
+    private http: HttpClient
+  ) { }
+}
--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,14 +1,20 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { Product } from '../products';
-
-
+import { Component, OnInit } from '@angular/core';
+import Product from '../product';
+import { Input } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
   styleUrls: ['./product-alerts.component.css']
 })
-export class ProductAlertsComponent {
-  @Input() product: Product | undefined;
+export class ProductAlertsComponent implements OnInit {
+  @Input() product!: Product;
   @Output() notify = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
 }
