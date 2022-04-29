2323ee278d45324b2023f22c2ab0b55e2ce9f74c
Ivan Chokoev <ivanchokoev96@gmail.com>
2022-04-29 18:57:59 +0300

    minor configurations

--- src/test.ts
+++ src/test.ts
@@ -26,8 +26,3 @@ const context = require.context('./', true, /\.spec\.ts$/);
 context.keys().map(context);
 
 
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
